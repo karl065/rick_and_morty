85c62a14277d032871c54be7a79bf776c70b020d
karl065 <karlos065@hotmail.com>
2023-05-15 21:49:29 -0500

    Agregando Estilos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,20 +12,17 @@ export default function Card({
 }) {
   return (
     <div className={styles.card}>
+      <img className={styles.imagenes} src={image} alt="image" />
+      <h2 className={styles.textoPrimario}>● {name}</h2>
+      <h3 className={styles.textoSecundario}>● Estado: {status}</h3>
+      <h3 className={styles.textoTerciario}>● Especie: {species}</h3>
+      <h3 className={styles.textoPrimario}>● Género: {gender}</h3>
+      <h3 className={styles.textoSecundario}>
+        ● Origen: {origin !== 'unknown' ? origin : 'sin registro '}
+      </h3>
       <button onClick={onClose} className={styles.boton}>
         X
       </button>
-      <h2 className={styles.textoPrimario}>{name}</h2>
-      <div className={styles.filas}>
-        <h3 className={styles.textoSecundario}>Estado: {status}</h3>
-        <h3 className={styles.textoTerciario}>Especie: {species}</h3>
-        <h3 className={styles.textoPrimario}>Género: {gender}</h3>
-        <h3 className={styles.textoSecundario}>
-          Origen:
-          {origin !== 'unknown' ? origin : 'sin registro '}
-        </h3>
-      </div>
-      <img className={styles.imagenes} src={image} alt="image" />
     </div>
   );
 }
